f544ae000d0d92cffc34dd8e760c7c611a479ecb
Duc Anh Babim <babim@matmagoc.com>
2017-11-16 02:02:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -49,3 +49,51 @@ define('CONFIG_VERSION', 26);
 ?>
 EOF
 fi
+
+# set ID docker run
+agid=${agid:-$auid}
+auser=${auser:-www-data}
+
+if [[ -z "${auid}" ]]; then
+  echo "start"
+elif [[ "$auid" = "0" ]] || [[ "$aguid" == "0" ]]; then
+	echo "run in user root"
+	auser=root
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	sed -i -e "/^user = .*/cuser = $auser" /etc/php5/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /etc/php5/php-fpm.conf
+	sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
+	sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
+elif id $auid >/dev/null 2>&1; then
+        echo "UID exists. Please change UID"
+else
+if id $auser >/dev/null 2>&1; then
+        echo "user exists"
+	sed -i -e "/^user = .*/cuser = $auser" /etc/php5/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /etc/php5/php-fpm.conf
+	sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
+	sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	# usermod alpine
+		deluser $auser && delgroup $auser
+		addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
+	# usermod ubuntu/debian
+		#usermod -u $auid $auser
+		#groupmod -g $agid $auser
+else
+        echo "user does not exist"
+	#export APACHE_RUN_USER=$auser
+	#export APACHE_RUN_GROUP=$auser
+	# create user alpine
+	addgroup -g $agid $auser && adduser -D -H -G $auser -s /bin/false -u $auid $auser
+	# create user ubuntu/debian
+	#groupadd -g $agid $auser && useradd --system --uid $auid --shell /usr/sbin/nologin -g $auser $auser
+	sed -i -e "/^user = .*/cuser = $auser" /etc/php5/php-fpm.conf
+	sed -i -e "/^group = .*/cgroup = $auser" /etc/php5/php-fpm.conf
+	sed -i -e "/^user .*/cuser  $auser;" /etc/nginx/nginx.conf
+	sed -i -e "/^#user .*/cuser  $auser;" /etc/nginx/nginx.conf
+fi
+
+fi
